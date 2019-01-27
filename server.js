7565a1ffe6429d359ac4c953bd1e4aba6f73731b
Lulin Zhang <zhang.lul@husky.neu.edu>
2019-01-26 23:37:57 -0800

    add server.js

---
+++ server.js
@@ -0,0 +1,31 @@
+// Get the dependencies
+
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Point static path to dist -- For building -- REMOVE
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
+const port = process.env.PORT || '3200';
+app.set('port', port);
+
+
+// Create HTTP server
+const server = http.createServer(app);
+server.listen( port , () => console.log('Running on port 3200'));
+
+//require('./assignment/app')(app);
