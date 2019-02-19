283be62160944e875b2236e25726b585fc951219
Lulin Zhang <zhang.lul@husky.neu.edu>
2019-02-19 05:14:46 -0800

    modify build

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Point static path to dist -- For building -- REMOVE
-app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static(path.join(__dirname, 'public')));
 
 // CORS
 app.use(function(req, res, next) {
