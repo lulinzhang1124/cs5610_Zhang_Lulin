d6bacdce76f71a48c6000b215010a2a380614c8c
Lulin Zhang <zhang.lul@husky.neu.edu>
2019-04-08 19:38:01 -0700

    fix db_url

--- assignment/model/model.js
+++ assignment/model/model.js
@@ -1,10 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
- var url = 'mongodb://localhost:27017/web5610';
- //var url = 'mongodb://normaluser:web5610@ds221416.mlab.com:21416/heroku_wc90b4tr';
- var mongo_url = process.env.MONGO_URL || "mongodb://localhost:27017/web5610";
-var client = mongoose.connect(url, { useNewUrlParser: true });
+
+var db_url = process.env.MONGO_URL || "mongodb://localhost:27017/web5610";
+var client = mongoose.connect(db_url, { useNewUrlParser: true });
 
 //Get the default connection
 const db = mongoose.connection;
